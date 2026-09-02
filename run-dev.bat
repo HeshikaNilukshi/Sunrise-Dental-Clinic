959f9af44b62fe3fc4b13d734c53265bb91ff6f3
HeshikaNilukshi <heshikafernando6@gmail.com>
2026-09-02 21:47:50 +0530

    first commit

---
+++ run-dev.bat
@@ -0,0 +1,11 @@
+@echo off
+start "Tailwind Watcher" /min cmd /k "tailwindcss.exe -i src/main/resources/static/css/input.css -o src/main/resources/static/css/output.css --watch"
+
+echo    ▲ Spring Boot
+echo    - Local:        http://localhost:8080
+echo    - Environments: default
+echo.
+echo  starting dev...
+echo.
+
+call .\mvnw.cmd spring-boot:run
